60b6a3ca877a46726f346da6e7b71b5b251e987d
Cleary.Johnny <cleary.johnny@hotmail.co.uk>
2020-05-03 12:56:50 +0000

    add css stylesheet

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $json = json_decode($jsondata,true);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Parsing JSON</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div>
